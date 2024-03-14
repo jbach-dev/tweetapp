44e5e34976a1547ae2a143bb92966d7052e9756d
Josephine Bach <josephinebach25@gmail.com>
2024-03-14 17:00:46 +0100

    app

--- pages/experience.py
+++ pages/experience.py
@@ -3,8 +3,8 @@ st.set_page_config(initial_sidebar_state="collapsed")
 
 st.markdown("""
     <style>
-        .st-b1 textarea{
-            color:white;
+        .st-emotion-cache-1veb5w3{
+            background-color:none;
         }
     </style>
     """, unsafe_allow_html=True)
