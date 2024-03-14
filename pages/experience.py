9b2c0853043ed5bc556027d37cb8f042d3059a77
Josephine Bach <josephinebach25@gmail.com>
2024-03-14 16:59:30 +0100

    app

--- pages/experience.py
+++ pages/experience.py
@@ -1,6 +1,15 @@
 import streamlit as st
 st.set_page_config(initial_sidebar_state="collapsed")
 
+st.markdown("""
+    <style>
+        .st-b1 textarea{
+            color:white;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+
+
 prompt = st.chat_input("Say something")
 if prompt:
     st.write(f"User has sent the following prompt: {prompt}")
