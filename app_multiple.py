7e3dc765ff3fae8137cc62ca947bbccc9f98b488
Josephine Bach <josephinebach25@gmail.com>
2024-03-14 15:56:16 +0100

    app

--- app_multiple.py
+++ app_multiple.py
@@ -36,7 +36,7 @@ st.markdown("""
 
         # .egzxvld4 .e1tzin5v0 .stMarkdown .e16nr0p34 p{
         # .e1f1d6gn2 .e1f1d6gn4 .stMarkdown .e1nzilvr5 p{
-        .e1nzilvr5 p{
+        .st-emotion-cache-1ibzyj6{
             border:1px solid rgba(49, 51, 63, 0.2);
             border-radius:20px;
             text-align:center;
