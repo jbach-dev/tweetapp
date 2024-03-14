a1f12e40100d9527cc98020ac4df61d36a89aed0
Josephine Bach <josephinebach25@gmail.com>
2024-03-14 17:15:30 +0100

    app

--- pages/experience.py
+++ pages/experience.py
@@ -5,30 +5,17 @@ st.set_page_config(initial_sidebar_state="collapsed")
 
 st.markdown("""
     <style>
-        .avatar{
-            display:none !important;
+        .e1nzilvr5{
+            width:70%;
         }
         .st-emotion-cache-1veb5w3{
             background-color:white;
         }
-        .msg{
-            background-color:white;
-        }
+
     </style>
     """, unsafe_allow_html=True)
 
-USER = "user"
-ASSISTANT = "assistant"
 
 prompt: str = st.chat_input("Enter a prompt here")
 
-if prompt:
-    st.chat_message(USER).write(prompt)
-    st.chat_message(ASSISTANT).write(f"You wrote {prompt}")
-
-
 st.write("RT @ABCWorldNews: MORE: Three people shot, suspect in custody in Los Angeles airport shooting!")
-
-# prompt = st.chat_input("Say something")
-# if prompt:
-#     st.write(f"User has sent the following prompt: {prompt}")
