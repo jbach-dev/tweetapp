bbbabd52ffda6ff3373e06db327b099e2f6338d8
Josephine Bach <josephinebach25@gmail.com>
2024-03-14 17:02:00 +0100

    app

--- pages/experience.py
+++ pages/experience.py
@@ -1,14 +1,19 @@
 import streamlit as st
+from streamlit_chat import message
+
 st.set_page_config(initial_sidebar_state="collapsed")
 
 st.markdown("""
     <style>
         .st-emotion-cache-1veb5w3{
-            background-color:none;
+            background-color:white;
         }
     </style>
     """, unsafe_allow_html=True)
 
+
+message("My message")
+message("Hello bot!", is_user=True)  # align's the message to the right
 
 prompt = st.chat_input("Say something")
 if prompt:
